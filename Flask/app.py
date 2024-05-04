cfb92aa769ab53c1713e38ec6129e508fa614fc8
Ryzen Fox <harlan.roberts.jr@icloud.com>
2024-05-04 19:40:30 -0400

    Corrected file paths and renamed things
    Corrected file paths that weren't updated previously as well as renamed some things to be more tidy

---
+++ Flask/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, Response
+import subprocess
+import time
+
+app = Flask(__name__)
+
+def execute_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    command_output, command_error = process.communicate()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/start')
+def start():
+    execute_command(['/home/ryzenfox/Astral-Moderation-v2/pythonvenv/python3.11.2-astral/bin/python', '/home/ryzenfox/Astral-Moderation-v2/bot.py'])
+    return 'Bot start completed successfully'
+
+@app.route('/stop')
+def stop():
+    execute_command(['pkill', '-f', 'bot.py'])
+    return 'Bot stop completed successfully'
+
+@app.route('/restart')
+def restart():
+    execute_command(['pkill', '-f', 'bot.py'])
+    time.sleep(5)
+    execute_command(['/home/ryzenfox/Astral-Moderation-v2/pythonvenv/python3.11.2-astral/bin/python', '/home/ryzenfox/Astral-Moderation-v2/bot.py'])
+    return 'Bot restart completed successfully'
+
+@app.route('/gitpull')
+def gitpull():
+    execute_command(['pkill', '-f', 'bot.py'])
+    time.sleep(2)
+    execute_command(['git', '-C', '/home/ryzenfox/Astral-Moderation-v2', 'pull'])
+    time.sleep(5)
+    execute_command(['/home/ryzenfox/Astral-Moderation-v2/pythonvenv/python3.11.2-astral/bin/python', '/home/ryzenfox/Astral-Moderation-v2/bot.py'])
+    return 'Main branch sync and bot restart completed successfully'
+
+@app.route('/systemrestart')
+def systemrestart():
+    execute_command(['sudo', 'systemctl', 'reboot'])
+    return 'System is restarting'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6040, debug=True)
